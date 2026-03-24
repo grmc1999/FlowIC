cb3d490dfb8daed5106eaa1502fd45a4c8bdc290
grmc1999 <grmc1999@gmail.com>
2026-03-24 12:16:44 -0300

    correction in wave

--- wave_firedrake.py
+++ wave_firedrake.py
@@ -326,6 +326,7 @@ if __name__ == "__main__":
     batch_size = args.n_samples
     rk_steps = 20
     loss_history = []
+    loss_ic_history = []
 
     for epoch in tqdm(range(args.epochs)):
         optimizer.zero_grad()
@@ -345,6 +346,9 @@ if __name__ == "__main__":
         loss.backward()
         optimizer.step()
 
+        loss_ic = torch.mean(torch.abs(pred_ic - gt_ic.unsqueeze(0))).detach().cpu().numpy()
+
+        loss_ic_history.append(loss_ic.item())
         loss_history.append(loss.item())
 
         if epoch % 4 == 0:
